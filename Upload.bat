1b4033989cd4574d19a5f0bc21833bc879076122
caxmirror <627085892@qq.com>
2023-02-22 09:50:55 +0800

    StudyRecord 1.0

--- Upload.bat
+++ Upload.bat
@@ -1,6 +1,8 @@
 git init
 git remote add origin https://github.com/caxmirror/StudyRecord.git
+git config user.name "caxmirror"
+git config user.email "627085892@qq.com"
 git add .
 git commit -m "StudyRecord 1.0"
-git push -u origin
+git push -u origin master
 pause
